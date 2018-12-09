164c94c19dfd805164bbcb6a9fce43d59447503e
Maciej Purski <maciej.purski@gmail.com>
2018-12-09 13:46:19 +0100

    Add Datafactory documentation

--- include/data-factory.h
+++ include/data-factory.h
@@ -5,18 +5,81 @@
 #include <limits>
 #include "device.h"
 
-// value - time
+/**
+ *  @brief Pair of value and sample time.
+ */
 using Data = std::pair<double, int>;
 
+/**
+ *  @class DataFactory
+ * 
+ *  @brief An abstract factory class allowing clients to querry the database.
+ *  Concrete class inheriting from that class shall implement connection with
+ *  a particular DataBase system.
+ */
 class DataFactory {
 public:
+
+    /**
+     *  @brief Determine if a connection with a data base is established.
+     *  Connection method is data-base dependent.
+     * 
+     *  @return True if a connection is established.
+     */
     virtual bool isConnected() = 0;
+
+    /**
+     *  @brief Get all devices in a database.
+     * 
+     *  @return A vector of all available devices.
+     */
     virtual std::vector<Device> getDevices() = 0;
+
+    /**
+     *  @brief Add a new device to the database.
+     * 
+     *  @param dev A reference to a new device.
+     */
     virtual void addDevice(const Device &dev) = 0;
+    
+    /**
+     *  @brief Remove existing device.
+     * 
+     *  @param deviceId Unique id of device which should be removed.
+     */
     virtual void removeDevice(int deviceId) = 0;
+
+    /**
+     *  @brief Get a single device
+     * 
+     *  @param id Unique id of device which should be removed.
+     * 
+     *  @return Device instance.
+     */
     virtual Device getDeviceById(int id) = 0;
+
+
+    /**
+     *  @brief Get data samples from the device.
+     *
+     *  @param deviceId Unique id of the device, which data we would like to get.
+     *
+     *  @param timeAfter Time in Unix time format.
+     *  @param timeBefore Time in Unix time format.
+     *
+     *  @param deviceId Unique id of device which should be removed.
+     * 
+     *  @return Vector of data samples.
+     */
     virtual std::vector<Data> getDeviceData(int deviceId,
                                             int timeAfter = 0, int timeBefore = std::numeric_limits<int>::max()) = 0;
+
+    /**
+     *  @brief Add a new data sample.
+     * 
+     *  @param deviceId Unique device id.
+     *  @param data A reference to new data sample.
+     */
     virtual void addData(int deviceId, const Data &data) = 0;
 };
 
--- include/pairly-exceptions.h
+++ include/pairly-exceptions.h
@@ -3,12 +3,22 @@
 
 #include <stdexcept>
 
-
+/**
+ * @class DataBaseException
+ * 
+ * @brief Exception thrown by classes inheriting from DataFactory, to indicate
+ * an in DataBase layer.
+ */
 class DataBaseException : public std::runtime_error {
 public:
     DataBaseException(const std::string &what) : std::runtime_error(what) { }
 };
 
+/**
+ * @class
+ * 
+ * @brief Exception thrown by the PairlyDB class
+ */
 class PairlyLibException : public std::runtime_error {
 public:
     PairlyLibException(const std::string &what) : std::runtime_error(what) { }
